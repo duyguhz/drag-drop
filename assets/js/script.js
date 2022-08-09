4e131ee332c892fcc6f436e9efdb4e09a66ce12e
duyguhz <duyghuhh@code.edu.az>
2022-08-09 11:11:42 +0400

    hw

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let zone=document.querySelector(".zone")
+let item1=document.querySelector(".circle-1")
+let item2=document.querySelector(".circle-2")
+let item3=document.querySelector(".circle-3")
+   
+ 
